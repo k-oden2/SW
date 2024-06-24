81efb55f03d8b0e46aa56f78b310dd1d6f788cef
k-oden2 <164519984+k-oden2@users.noreply.github.com>
2024-06-25 05:04:54 +0700

    Update wiu.sh

--- wiu.sh
+++ wiu.sh
@@ -5,10 +5,9 @@
 sudo dpkg --add-architecture i386
 wget -nc https://dl.winehq.org/wine-builds/winehq.key
 sudo apt-key add winehq.key
-#
-sudo apt-get update 
-sudo apt install wine32 -y
-
+sudo add-apt-repository -y 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main'
+sudo apt update
+sudo apt install -y --install-recommends winehq-stable
 # WINE UPDATE
 cd
 wine --version
@@ -32,8 +31,13 @@ sudo apt-get install -y winetricks
 sudo apt install -y zenity
 wineserver -k
 winetricks --self-update
-winetricks vcrun2019 gdiplus quartz ffdshow wmp10 vd=600x650 unattended alldlls=builtin
-
+winetricks --unattended vd=600x650 alldlls=builtin
+#
+wine reg add "HKCU\\Software\\Wine\\Direct3D" /v UseGLSL /t REG_SZ /d disabled /f
+wine reg add "HKCU\\Software\\Wine\\Direct3D" /v StrictDrawOrdering /t REG_SZ /d enabled /f
+wine reg add "HKCU\\Software\\Wine\\Direct3D" /v DirectDrawRenderer /t REG_SZ /d gdi /f
+wine reg add "HKCU\\Software\\Wine\\Direct3D" /v VideoMemorySize /t REG_SZ /d 2048 /f
+wineserver -k
 # ADD LC JP
 sudo apt-get install language-pack-ja -y
 sudo apt update
@@ -41,18 +45,12 @@ wineboot
 
 # EXPLORER
 echo -e "[Desktop Entry]\nName=Explorer\nExec=env wine explorer.exe\nType=Application\nStartupNotify=true\nPath=/home/user/.wine/drive_c:/windows\nStartupWMClass=explorer.exe\nIcon=1CD8_rundll32.0\nComment=\nTerminal=false" > ~/Desktop/Explorer.desktop
-
 # EXPLORER JP
 echo -e "[Desktop Entry]\nName=Explorer JP\nExec=env LANG=\"ja_JP.UTF8\" wine explorer.exe\nType=Application\nStartupNotify=true\nPath=/home/user/.wine/drive_c:/windows\nStartupWMClass=explorer.exe\nIcon=1CD8_rundll32.0\nComment=\nTerminal=false" > ~/Desktop/Explorer_JP.desktop
 
 # PERMISSION 
 chmod +x ~/Desktop/Explorer.desktop
 chmod +x ~/Desktop/Explorer_JP.desktop
-
-# APP
-cd
-wget -O vc-all.zip https://sg1-dl.techpowerup.com/files/n7z_3HMAu8vHUOR6-fRd_A/1709216034/Visual-C-Runtimes-All-in-One-Feb-2024.zip
-
 # CLEAN
 sudo apt-get clean
 sudo apt-get autoclean -y
